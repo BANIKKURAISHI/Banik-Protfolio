317d817f015a12635057075618b23a4a96a91019
BANIKKURAISHI <mdkuraishirahmanbanik@gmail.com>
2023-12-23 20:54:35 +0600

    first commit

---
+++ src/Components/Route/Route.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom"
+import Home from "../A1(Index)/Home"
+
+const myCreateRoute=createBrowserRouter([
+    {
+    path:'/',
+    element:<Home></Home>
+    
+     }
+])
+
+
+export default myCreateRoute
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import myCreateRoute from './Components/Route/Route'
+import { RouterProvider } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ <RouterProvider router={myCreateRoute} />
+  </React.StrictMode>,
+)
